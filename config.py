101576665ea56c0b87c5f644bd7ae07d42fb01e6
Waulkin <tre.stamps@outlook.com>
2025-03-31 13:58:16 -0400

    fix: correct static folder path separator in Flask app configuration

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 from flask.helpers import send_from_directory
 
-app = Flask(__name__, static_folder='frontend/dist', static_url_path='')
+app = Flask(__name__, static_folder='frontend\dist', static_url_path='')
 CORS(app)
 
 @app.after_request
