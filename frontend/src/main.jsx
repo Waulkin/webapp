68b7d763e1ca92ac97fb46e3202b704eb5eb3038
Waulkin <tre.stamps@outlook.com>
2025-03-31 18:23:06 -0400

    fix: remove App component from main rendering in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import FaceApp from './FaceApp.jsx'
-import App from './App.jsx'
+
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const CLIENT_ID="21437432952-sp1i8aon5a53reh7e92oh3a1qbb3vm1a.apps.googleusercontent.com"
@@ -10,7 +10,7 @@ const CLIENT_ID="21437432952-sp1i8aon5a53reh7e92oh3a1qbb3vm1a.apps.googleusercon
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={CLIENT_ID}> 
-    {/*<App />*/}
+    
     <FaceApp /> 
     </GoogleOAuthProvider>
   
