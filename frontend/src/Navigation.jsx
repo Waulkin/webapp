c92922096ba22d9017dfe717161e8dc8e05df3ae
Waulkin <tre.stamps@outlook.com>
2025-03-31 17:50:56 -0400

    fix: update route paths in App and Navigation components to include 'app/' prefix

--- frontend/src/Navigation.jsx
+++ frontend/src/Navigation.jsx
@@ -5,11 +5,11 @@ const Navigation = () => {
             <h1>Navigation Placeholder Name</h1>
             <div className="nav-links">
             
-                <Link to="/">Home</Link>
+                <Link to="app/">Home</Link>
                 &nbsp;&nbsp;&nbsp;
-                <Link to="/user">User</Link>
+                <Link to="app/user">User</Link>
                 &nbsp;&nbsp;&nbsp;
-                <Link to="/inventory">Inventory</Link>
+                <Link to="app/inventory">Inventory</Link>
             </div>
         </nav>
     );
