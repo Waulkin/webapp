237066eb61933e73f1d43c8336889b3c62ccc0ad
Waulkin <tre.stamps@outlook.com>
2025-03-31 17:16:04 -0400

    fix: switch main application component to FaceApp in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,8 @@ const CLIENT_ID="21437432952-sp1i8aon5a53reh7e92oh3a1qbb3vm1a.apps.googleusercon
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={CLIENT_ID}> 
-    <App />
-    {/*<FaceApp />*/} 
+    {/*<App />*/}
+    <FaceApp /> 
     </GoogleOAuthProvider>
   
   </StrictMode>,
